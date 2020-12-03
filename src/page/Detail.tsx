516f54420ca1a24ce14dbe64c3bc9815e8af11ca
johnson.qiao <johnson.qiao@derbysoft.net>
2020-12-03 17:50:01 +0800

    test

--- src/page/Detail.tsx
+++ src/page/Detail.tsx
@@ -1,129 +1,131 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
-import { Table } from 'react-bootstrap';
-import {connect} from "react-redux";
-import Auth from '../component/Auth'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Table } from "react-bootstrap";
+import { connect } from "react-redux";
+import Auth from "../component/Auth";
 
 const columns = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
+  {
+    title: "Name",
+    dataIndex: "name",
+  },
+  {
+    title: "Chinese Score",
+    dataIndex: "chinese",
+    sorter: {
+      compare: (a: any, b: any) => a.chinese - b.chinese,
+      multiple: 3,
     },
-    {
-        title: 'Chinese Score',
-        dataIndex: 'chinese',
-        sorter: {
-            compare: (a: any, b: any) => a.chinese - b.chinese,
-            multiple: 3,
-        },
+  },
+  {
+    title: "Math Score",
+    dataIndex: "math",
+    sorter: {
+      compare: (a: any, b: any) => a.math - b.math,
+      multiple: 2,
     },
-    {
-        title: 'Math Score',
-        dataIndex: 'math',
-        sorter: {
-            compare: (a: any, b: any) => a.math - b.math,
-            multiple: 2,
-        },
+  },
+  {
+    title: "English Score",
+    dataIndex: "english",
+    sorter: {
+      compare: (a: any, b: any) => a.english - b.english,
+      multiple: 1,
     },
-    {
-        title: 'English Score',
-        dataIndex: 'english',
-        sorter: {
-            compare: (a: any, b: any) => a.english - b.english,
-            multiple: 1,
-        },
-    },
+  },
 ];
 
 const data = [
-    {
-        key: '1',
-        name: 'John Brown',
-        chinese: 98,
-        math: 60,
-        english: 70,
-    },
-    {
-        key: '2',
-        name: 'Jim Green',
-        chinese: 98,
-        math: 66,
-        english: 89,
-    },
-    {
-        key: '3',
-        name: 'Joe Black',
-        chinese: 98,
-        math: 90,
-        english: 70,
-    },
-    {
-        key: '4',
-        name: 'Jim Red',
-        chinese: 88,
-        math: 99,
-        english: 89,
-    },
+  {
+    key: "1",
+    name: "John Brown",
+    chinese: 98,
+    math: 60,
+    english: 70,
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    chinese: 98,
+    math: 66,
+    english: 89,
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    chinese: 98,
+    math: 90,
+    english: 70,
+  },
+  {
+    key: "4",
+    name: "Jim Red",
+    chinese: 88,
+    math: 99,
+    english: 89,
+  },
 ];
-
+interface Fish {
+  name: string;
+}
 function onChange(pagination: any, filters: any, sorter: any, extra: any) {
-    console.log('params', pagination, filters, sorter, extra);
+  console.log("params", pagination, filters, sorter, extra);
 }
 
-class Detail extends React.Component<any, any>{
-    componentWillMount() {
-        console.log('check user2', this.props.data);
-    }
-    render() {
-        return(
-            <Auth auth-id={'page-detail'}>
-                <Table responsive>
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                    </tr>
-                    </tbody>
-                </Table>
-            </Auth>
-        )
-    }
+class Detail extends React.Component<any, any> {
+  componentWillMount() {
+    console.log("check user2", this.props.data);
+  }
+  render() {
+    return (
+      <Auth auth-id={"page-detail"}>
+        <Table responsive>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Table heading</th>
+              <th>Table heading</th>
+              <th>Table heading</th>
+              <th>Table heading</th>
+              <th>Table heading</th>
+              <th>Table heading</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Auth>
+    );
+  }
 }
-const mapState = (state: any)=>({
-    data:state
+const mapState = (state: any) => ({
+  data: state,
 });
-export default connect(mapState)(Detail)
+export default connect(mapState)(Detail);
